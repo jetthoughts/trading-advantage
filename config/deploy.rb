07eb868b13172bca43df4f526ceaf120eacdfb3c
Alexey Kisel <aki@jetthoughts.com>
2013-10-13 19:26:45 +0300

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,15 +4,17 @@ set :repository, "git@github.com:jetthoughts/trading-advantage.git"
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 
                                              #role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "31.220.63.160", :primary => true # This may be the same as your `Web` server
+role :app, "198.61.172.139", :primary => true # This may be the same as your `Web` server
                                              #role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
                                              #role :db,  "your slave db-server here"
 set :use_sudo, false
 default_run_options[:pty]   = true
 ssh_options[:forward_agent] = true
-ssh_options[:port]          = 2212
+#ssh_options[:port]          = 2212
 set :deploy_to, "/data/apps/#{application}"
-set :user, "rails"
+#TODO: Need deploy from www-data. Ask for password
+set :user, "root"
+set :password, "ehD9ZC4HAsU2"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -31,5 +33,15 @@ set :user, "rails"
 namespace :deploy do
   task :uname do
     run "uname -a"
+  end
+
+  desc 'sync folders with wordpress main project'
+  task :copy_folders  do
+    dest_path = '/var/www/vhosts/www.tradingadvantage.com/http_docs/wp-content'
+    #run "#{try_sudo} cp -rf #{release_path}/wordpress/wp-content/* #{dest_path}/"
+    %w(plugins themes).each do |folder_name|
+      run "#{try_sudo} cp -rf #{current_path}/wordpress/wp-content/#{folder_name} #{dest_path}/#{folder_name}"
+      run "#{try_sudo} chown -R www-data:www-data #{dest_path}/#{folder_name}"
+    end
   end
 end
